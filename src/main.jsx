6709f5a9b060491d9eaf09eef275691730d0514b
ManavTriv <trivedimanav2003@gmail.com>
2025-05-06 17:39:30 +1000

    testing env vars

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import "./styles/index.css";
 import App from "./App.jsx";
 import "./api/parseConfig.js";
 
+console.log("Parse App ID:", import.meta.env.VITE_PARSE_APP_ID);
+console.log("Parse JS KEY:", import.meta.env.VITE_PARSE_JS_KEY);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
